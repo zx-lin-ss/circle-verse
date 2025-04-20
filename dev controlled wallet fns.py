20bc8905f56b6a4792a698fd978027392f2e34c2
zx-lin-ss <franciszxlin@gmail.com>
2025-04-20 16:06:51 -0400

    Initial commit

---
+++ dev controlled wallet fns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 15:55:22 2025
+
+@author: jessicahert
+"""
+
+from circle.web3 import utils
+# This will print a new entity secret in the terminal
+utils.generate_entity_secret()
